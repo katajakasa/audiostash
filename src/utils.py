06039676203781cae45044deacd5eb4b38ae4cfb
Tuomas Virtanen <katajakasa@gmail.com>
2015-07-24 02:34:01 +0300

    Small changes to commandline utilities

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import sys
 import getpass
 from passlib.hash import pbkdf2_sha256
 from common.tables import USERLEVELS, database_init, session_get, User
+from sqlalchemy.exc import IntegrityError
+
 
 def create_admin(_username, _password):
     database_init(settings.DBFILE)
@@ -12,8 +14,11 @@ def create_admin(_username, _password):
     s = session_get()
     user = User(username=_username, password=pw_hash, level=USERLEVELS['admin'])
     s.add(user)
-    s.commit()
-    print("User '{}' created.".format(username))
+    try:
+        s.commit()
+        print("User '{}' created.".format(_username))
+    except IntegrityError:
+        print("User {} already exists.".format(_username))
 
 if 'create_admin' in sys.argv:
     username = raw_input("Username: ")
@@ -21,6 +26,11 @@ if 'create_admin' in sys.argv:
     create_admin(username, password)
     exit(0)
 
+if 'create_test_admin' in sys.argv:
+    create_admin('admin', 'admin')
+    exit(0)
+
 print("AudioStash utilities")
 print("create_admin - Creates a new admin user")
+print("create_test_admin - Create a test admin user")
 exit(0)
