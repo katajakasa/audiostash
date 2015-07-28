c1296a0af702a86276bcc94cba31d34b21d6be32
Tuomas Virtanen <katajakasa@gmail.com>
2015-07-28 05:30:22 +0300

    Client side cleanups + add playlist management buttons to album view

--- src/public/static/custom/app.js
+++ src/public/static/custom/app.js
@@ -1,38 +1,38 @@
 'use strict';
 
 var app = angular.module(
-  'audiostash',
-  [
-    'ngRoute',
-    'indexedDB',
-    'ng.sockjs',
-    'ui.bootstrap',
-    'ui.grid'
-  ]
+    'audiostash',
+    [
+        'ngRoute',
+        'indexedDB',
+        'ng.sockjs',
+        'ui.bootstrap',
+        'ui.grid'
+    ]
 );
 
-app.config(function($indexedDBProvider) {
-  $indexedDBProvider
-    .connection('audiostash')
-    .upgradeDatabase(1, function(event, db, tx) {
-      var artist_store        = db.createObjectStore("artist", { keyPath: "id" });
-      var album_store         = db.createObjectStore("album", { keyPath: "id" });
-      var directory_store     = db.createObjectStore("directory", { keyPath: "id" });
-      var playlist_store      = db.createObjectStore("playlist", { keyPath: "id" });
-      var playlisttrack_store = db.createObjectStore("playlisttrack", { keyPath: "id" });
-      var track_store         = db.createObjectStore("track", { keyPath: "id" });
-      var config_store        = db.createObjectStore("config", { keyPath: "id" });
+app.config(function ($indexedDBProvider) {
+    $indexedDBProvider
+        .connection('audiostash')
+        .upgradeDatabase(1, function (event, db, tx) {
+            var artist_store = db.createObjectStore("artist", {keyPath: "id"});
+            var album_store = db.createObjectStore("album", {keyPath: "id"});
+            var directory_store = db.createObjectStore("directory", {keyPath: "id"});
+            var playlist_store = db.createObjectStore("playlist", {keyPath: "id"});
+            var playlisttrack_store = db.createObjectStore("playlisttrack", {keyPath: "id"});
+            var track_store = db.createObjectStore("track", {keyPath: "id"});
+            var config_store = db.createObjectStore("config", {keyPath: "id"});
 
-      album_store.createIndex("artist","artist", { unique: false });
-      playlisttrack_store.createIndex("playlist","playlist", { unique: false });
-      playlisttrack_store.createIndex("track","track", { unique: false });
-      track_store.createIndex("album_id","album_id", { unique: false });
-      track_store.createIndex("dir","dir", { unique: false });
-      track_store.createIndex("artist_id","artist_id", { unique: false });
-      track_store.createIndex("track", "track", { unique: false });
-      track_store.createIndex("disctrack", ['disc','track'], {unique: false});
-      config_store.createIndex("key","key", { unique: true });
-    });
+            album_store.createIndex("artist", "artist", {unique: false});
+            playlisttrack_store.createIndex("playlist", "playlist", {unique: false});
+            playlisttrack_store.createIndex("track", "track", {unique: false});
+            track_store.createIndex("album_id", "album_id", {unique: false});
+            track_store.createIndex("dir", "dir", {unique: false});
+            track_store.createIndex("artist_id", "artist_id", {unique: false});
+            track_store.createIndex("track", "track", {unique: false});
+            track_store.createIndex("disctrack", ['disc', 'track'], {unique: false});
+            config_store.createIndex("key", "key", {unique: true});
+        });
 });
 
 // Sockjs
@@ -42,41 +42,41 @@ app.value('ngSockUrl', '/sock');
 // URLs
 // (custom) requireLogin: True for logincheck
 app.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/login', {
-        templateUrl: '/partials/login.html',
-          controller: 'LoginController'
-      }).
-      when('/logout', {
-        templateUrl: '/partials/logout.html',
-        controller: 'LogoutController'
-      }).
-      when('/albums', {
-        templateUrl: '/partials/albums.html',
-        controller: 'AlbumsController',
-        requireLogin: true
-      }).
-      when('/album/:artistId/:albumId', {
-        templateUrl: '/partials/album.html',
-        controller: 'AlbumController',
-        requireLogin: true
-      }).
-      when('/tracks', {
-        templateUrl: '/partials/tracks.html',
-        controller: 'TrackController',
-        requireLogin: true
-      }).
-      when('/audiobooks', {
-        templateUrl: '/partials/audiobooks.html',
-        requireLogin: true
-      }).
-      when('/playlists', {
-        templateUrl: '/partials/playlists.html',
-        requireLogin: true
-      }).
-      otherwise({
-        redirectTo: '/login'
-      });
-  }
+    function ($routeProvider) {
+        $routeProvider.
+            when('/login', {
+                templateUrl: '/partials/login.html',
+                controller: 'LoginController'
+            }).
+            when('/logout', {
+                templateUrl: '/partials/logout.html',
+                controller: 'LogoutController'
+            }).
+            when('/albums', {
+                templateUrl: '/partials/albums.html',
+                controller: 'AlbumsController',
+                requireLogin: true
+            }).
+            when('/album/:artistId/:albumId', {
+                templateUrl: '/partials/album.html',
+                controller: 'AlbumController',
+                requireLogin: true
+            }).
+            when('/tracks', {
+                templateUrl: '/partials/tracks.html',
+                controller: 'TrackController',
+                requireLogin: true
+            }).
+            when('/audiobooks', {
+                templateUrl: '/partials/audiobooks.html',
+                requireLogin: true
+            }).
+            when('/playlists', {
+                templateUrl: '/partials/playlists.html',
+                requireLogin: true
+            }).
+            otherwise({
+                redirectTo: '/login'
+            });
+    }
 ]);
---
+++ src/public/static/custom�constants.js
@@ -0,0 +1,27 @@
+'use strict';
+
+app.constant('AUTH_EVENTS', {
+    loginSuccess: 'auth-login-success',
+    loginFailed: 'auth-login-failed',
+    logoutBegin: 'auth-logout-begin',
+    logoutSuccess: 'auth-logout-success',
+    sessionTimeout: 'auth-session-timeout',
+    notAuthenticated: 'auth-not-authenticated',
+    notAuthorized: 'auth-not-authorized'
+});
+
+app.constant('PLAYLIST_EVENTS', {
+    refresh: 'playlist-refresh'
+});
+
+app.constant('SYNC_EVENTS', {
+    started: 'sync-started',
+    stopped: 'sync-stopped',
+    newData: 'sync-newdata'
+});
+
+app.constant('USERLEVELS', [
+    'none',
+    'user',
+    'admin'
+]);
